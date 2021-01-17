4ec867affa8e2a16640e752a91355ba2a4a75a5c
DragosCocirlea <dragos.cocirlea@gmail.com>
2021-01-17 19:38:10 +0200

    docker-compose update

--- server/server.py
+++ server/server.py
@@ -33,46 +33,5 @@ api.add_resource(resources.BarberServiceData, '/barbers_services')
 api.add_resource(resources.TimeData, '/time')
 api.add_resource(resources.AppointmentData, '/appointment')
 
-@app.route('/')
-@app.route('/inspect/users')
-def show_users():
-    columns = ['email', 'name', 'phone', 'birthday', 'rating']
-    users = []
-    for user in UserModel.query.all():
-        users.append([user.id, user.name, user.phone, user.birthday, str(user.rating)])
-    return render_template('table.html', records=users, colnames=columns, tablename='USERS', size=len(columns))
-
-@app.route('/inspect/barbershops')
-def show_barbershops():
-    columns = ['id', 'name', 'address', 'rating', 'coordX', 'coordY']
-    barbershops = []
-    for bbs in BarbershopModel.query.all():
-        barbershops.append([bbs.id, bbs.name, bbs.address, str(bbs.rating), str(bbs.coordX), str(bbs.coordY)])
-    return render_template('table.html', records=barbershops, colnames=columns, tablename='BARBERSHOPS', size=len(columns))
-
-@app.route('/inspect/barbers')
-def show_barbers():
-    columns = ['id', 'name', 'rating', 'bbs_id']
-    barbers = []
-    for barber in BarberModel.query.all():
-        barbers.append([barber.id, barber.name, str(barber.rating), barber.bbs_id])
-    return render_template('table.html', records=barbers, colnames=columns, tablename='BARBERS', size=len(columns))
-
-@app.route('/inspect/services')
-def show_services():
-    columns = ['id', 'name', 'price', 'bbs_id']
-    services = []
-    for serv in ServiceModel.query.all():
-        services.append([serv.id, serv.name, str(serv.price), serv.bbs_id])
-    return render_template('table.html', records=services, colnames=columns, tablename='SERVICES', size=len(columns))
-
-@app.route('/inspect/appointments')
-def show_appointments():
-    columns = ['id', 'date', 'time', 'barber_id', 'client_id', 'service_id']
-    appointments = []
-    for app in AppointmentModel.query.all():
-        appointments.append([app.id, app.date, app.time, app.barber_id, app.client_id, app.service_id])
-    return render_template('table.html', records=appointments, colnames=columns, tablename='APPOINTMENTS', size=len(columns))
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
